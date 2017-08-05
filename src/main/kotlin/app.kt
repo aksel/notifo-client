e632c5eab0c3f296187a36fbac844b9f3530f19e
torgard <aksel@akseltorgard.com>
2017-08-05 16:51:30 +0100

    Hardcoded client connection. Also added org.json to dependencies, for future usage.

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,3 +1,14 @@
+import io.socket.client.IO
+import io.socket.client.Socket
+
 fun main(args: Array<String>) {
-    println("Hello world")
+  println("Notifo client")
+  connect()
+}
+
+fun connect() {
+  val socket = IO.socket("http://localhost:4000?user=aksel")
+  socket.connect()
+      .on(Socket.EVENT_CONNECT, { println("connected") })
+      .on(Socket.EVENT_DISCONNECT, { println("disconnected") })
 }
