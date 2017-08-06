05a3f61c0f88efa9f89f0711b5833ec680063688
torgard <aksel@akseltorgard.com>
2017-08-06 22:25:39 +0200

    Print notifications on receive. Get all on connect.

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,5 +1,7 @@
 import io.socket.client.IO
 import io.socket.client.Socket
+import org.json.JSONArray
+import org.json.JSONObject
 
 fun main(args: Array<String>) {
   println("Notifo client")
@@ -9,6 +11,19 @@ fun main(args: Array<String>) {
 fun connect() {
   val socket = IO.socket("http://localhost:4000?user=aksel")
   socket.connect()
-      .on(Socket.EVENT_CONNECT, { println("connected") })
+      .on(Socket.EVENT_CONNECT, {
+        println("connected")
+        socket.emit("get")
+      })
+      .on("notifications", { data ->
+        val notifications = data[0] as JSONArray
+        for (notification in notifications) {
+          println("Notification: $notification")
+        }
+      })
+      .on("notification", { data ->
+        val notification = data[0] as JSONObject
+        println("Notification: $notification")
+      })
       .on(Socket.EVENT_DISCONNECT, { println("disconnected") })
 }
