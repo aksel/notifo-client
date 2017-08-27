d2dca1941a38f1e61410fe17a1f8bb8b6402c157
torgard <aksel@akseltorgard.com>
2017-08-27 20:42:35 +0200

    Added action listener to exit popup item.

--- src/main/kotlin/UI.kt
+++ src/main/kotlin/UI.kt
@@ -1,11 +1,8 @@
 package main.kotlin
 
 import java.awt.*
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import javax.swing.JFrame
 import javax.swing.JLabel
-import javax.swing.SwingUtilities
 import kotlin.system.exitProcess
 
 class UI : JFrame() {
